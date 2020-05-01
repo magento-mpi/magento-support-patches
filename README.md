983fcbd115d056b6371d55d6e3abc2b7c510b824
Viktor Tymchynskyi <vtymchynskyi@magento.com>
2020-05-01 17:55:46 -0500

    - Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Magento Support Patches
+
+## Welcome
+The Magento Support Patches package is a set of patches used to deliver critical fixes.
---
+++ bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+require __DIR__ . '/autoload.php';
+
+use Magento\SupportPatches\App\Container;
+
+// It's a fork 1.0.1
+return new Container(__DIR__, BP);
